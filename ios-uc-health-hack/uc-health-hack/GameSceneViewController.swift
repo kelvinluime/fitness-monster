0b92d4783606ebfe15e7c1e7083bf262fc6cfb84
kelvinluime <kelflo1120@gmail.com>
2018-10-06 11:06:56 -0700

    add initial assets

---
+++ ios-uc-health-hack/uc-health-hack/GameSceneViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GameSceneViewController.swift
+//  uc-health-hack
+//
+//  Created by Kelvin Lui on 10/6/18.
+//  Copyright © 2018 Kelvin Lui. All rights reserved.
+//
+
+import UIKit
+
+class GameSceneViewController: UIViewController {
+    let monsterImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = 
+        return iv
+    }()
+
+    override func viewDidLoad() {
+
+    }
+}
