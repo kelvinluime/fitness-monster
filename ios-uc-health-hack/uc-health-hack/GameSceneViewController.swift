9bcdbb433f1aef8228198cb7e97be9b29e9d9ac1
kelvinluime <kelflo1120@gmail.com>
2018-10-06 12:04:47 -0700

    install firebase pod

--- ios-uc-health-hack/uc-health-hack/GameSceneViewController.swift
+++ ios-uc-health-hack/uc-health-hack/GameSceneViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class GameSceneViewController: UIViewController {
+    let profileButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(handleProfile), for: UIControl.Event.touchUpInside)
+        button.setImage(UIImage(imageLiteralResourceName: "profile"), for: UIControl.State.normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    let promptLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.attributedText = NSAttributedString(string: "you can do it!", attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14),
+            NSAttributedString.Key.foregroundColor : UIColor.white
+            ])
+        return label
+    }()
+
     let monsterImageView: UIImageView = {
         let iv = UIImageView()
         iv.image = UIImage(imageLiteralResourceName: "monster")
@@ -20,6 +38,10 @@ class GameSceneViewController: UIViewController {
         let tf = UITextView()
         return tf
     }()
+
+    @objc func handleProfile() {
+
+    }
 
     override func viewDidLoad() {
         view.backgroundColor = .white
@@ -38,12 +60,19 @@ class GameSceneViewController: UIViewController {
         topViewContainer.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         topViewContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
 
+        topViewContainer.addSubview(profileButton)
+        profileButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        profileButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        profileButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        profileButton.widthAnchor.constraint(equalToConstant: 24).isActive = true
+
         topViewContainer.addSubview(monsterImageView)
         monsterImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         monsterImageView.centerYAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height*0.25).isActive = true 
         monsterImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         monsterImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
 
-        view.addSubview(monsterImageView)
+            
+
     }
 }
