ac904d34dfd4f794ec5221c31b64811f873e111e
Rix Lai <rixktl@gmail.com>
2018-10-06 12:28:52 -0700

    Add firebase with plist

--- ios-uc-health-hack/uc-health-hack/AppDelegate.swift
+++ ios-uc-health-hack/uc-health-hack/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = GameSceneViewController()
         window?.makeKeyAndVisible()
+
+        FirebaseApp.configure()
 
         return true
     }
